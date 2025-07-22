34050725e5efc588587ec6d00507db94b748c0bc
Sakshi0781 <yelwande.sy@pg.com>
2025-07-22 19:38:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from functools import reduce
 
 # Set the API key for Google Serper
 os.environ["SERPER_API_KEY"] = "5c96203175e8f497ae8d035e3fa7485ce9f01d4e"
-os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "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"
 # Define the keywords for each brand
 # Default keywords for each brand
 DEFAULT_PG_KEYWORDS = {
